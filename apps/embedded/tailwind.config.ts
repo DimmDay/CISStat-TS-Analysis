1e55c39015f7b1339225459a9c0483593b7396f4
DimmDay <dimm.day@gmail.com>
2026-07-27 04:22:34 -0700

    embedded + standalone v.1_27.07.26_14:22

--- apps/embedded/tailwind.config.ts
+++ apps/embedded/tailwind.config.ts
@@ -1,5 +1,5 @@
 import type { Config } from "tailwindcss";
-import { tailwindPreset } from "@cisstat/ui";
+import tailwindPreset from "@cisstat/ui/tailwind-preset";
 
 const config: Config = {
   presets: [tailwindPreset as Config],
