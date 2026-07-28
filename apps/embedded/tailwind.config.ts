29c89499d1a962c748ec42984c5b348f58e51e58
DimmDay <dimm.day@gmail.com>
2026-07-28 17:22:36 +0300

    28.07.26_17:22

--- apps/embedded/tailwind.config.ts
+++ apps/embedded/tailwind.config.ts
@@ -1,11 +1,12 @@
+import path from "path";
 import type { Config } from "tailwindcss";
 import tailwindPreset from "@cisstat/ui/tailwind-preset";
 
 const config: Config = {
   presets: [tailwindPreset as Config],
   content: [
-    "./app/**/*.{ts,tsx}",
-    "../../packages/ui/**/*.{ts,tsx}",
+    path.resolve(__dirname, "app/**/*.{ts,tsx}"),
+    path.resolve(__dirname, "../../packages/ui/**/*.{ts,tsx}"),
   ],
 };
 
